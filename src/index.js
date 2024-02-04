c2297fcc96944f573195e72122d30b5faa8aef31
Dhananjoy Mahata <dhananjoymahata43@gmail.com>
2024-02-04 22:55:05 +0530

    utils added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import dotenv from "dotenv";
 import { ConnectDB } from "./db/db.js";
+import { app } from "./app.js";
 
 dotenv.config()
 
 
-ConnectDB();
+ConnectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server running on  http://localhost:${process.env.PORT || 8000}`);
+    })
+}).catch((error) => {
+    console.log("MONGODB connection error!!! ", error);
+});
