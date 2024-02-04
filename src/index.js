8892ab8fb53591c42ad8e21d8d2b734fd4cf9df3
Dhananjoy Mahata <dhananjoymahata43@gmail.com>
2024-02-05 02:10:55 +0530

    all setup configration complete

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 ConnectDB().then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server running on  http://localhost:${process.env.PORT || 8000}`);
+        console.log(`Server is running on http://localhost:${process.env.PORT || 8000}`);
     })
 }).catch((error) => {
     console.log("MONGODB connection error!!! ", error);
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,12 @@
-export const asyncHandler = (fn) => async (req, res, next) => {
-    try {
-        await fn(req, res, next);
-    } catch (error) {
-        res.status(error.code || 500).json({
-            success: false,
-            error: error.message || 'Server Error in asyncHandler'
-        })
+export const asyncHandler = (fn) => {
+    return async (req, res, next) => {
+        try {
+            await fn(req, res, next);
+        } catch (error) {
+            res.status(error.code || 500).json({
+                success: false,
+                error: error.message || 'Server Error in asyncHandler'
+            })
+        }
     }
 }
