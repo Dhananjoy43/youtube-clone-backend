9d5ebc1e3fc22e846b2a13d52a851a409ad334e1
Dhananjoy Mahata <dhananjoymahata43@gmail.com>
2024-02-24 03:09:55 +0530

    moved index.js file to root

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
-import { ConnectDB } from "./db/db.js";
-import { app } from "./app.js";
+import { ConnectDB } from "./src/db/db.js";
+import { app } from "./src/app.js";
 
 dotenv.config()
 
