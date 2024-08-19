42f995af560330ed3bdd9860c760ae7d9a8c93f6
Dhananjoy Mahata <dhananjoymahata43@gmail.com>
2024-08-19 20:14:36 +0530

    Updated: Register

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -18,12 +18,7 @@ import {
 
 const router = Router();
 
-router.route("/register").post(
-    upload.fields([
-        { name: 'avatar', maxCount: 1 },
-        { name: 'coverImage', maxCount: 1 }
-    ]),
-    registerUser);
+router.route("/register").post(registerUser);
 
 router.route("/login").post(loginUser);
 
